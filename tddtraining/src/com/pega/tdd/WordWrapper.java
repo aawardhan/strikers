aa07922e4a3f74f6b08d76e62d507b3327ee4298
Aawardhan Logandan <aawardhan.logandan@in.pega.com>
2017-02-09 19:53:46 +0530

    trial

---
+++ tddtraining/src/com/pega/tdd/WordWrapper.java
@@ -0,0 +1,8 @@
+package com.pega.tdd;
+
+public class WordWrapper {
+
+	public String concatenate(String one, String two){
+        return one + two;
+    }
+}
---
+++ tddtraining/src/com/pega/tdd/WordWrapperTest.java
@@ -0,0 +1,14 @@
+package com.pega.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordWrapperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
